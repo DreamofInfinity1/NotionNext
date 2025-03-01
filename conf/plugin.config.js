743387375465a3af821dd455693f94bac20dd13b
DreamofInfinity1 <129919780+DreamofInfinity1@users.noreply.github.com>
2025-03-01 15:03:26 +0800

    Update plugin.config.js

--- conf/plugin.config.js
+++ conf/plugin.config.js
@@ -23,7 +23,7 @@ module.exports = {
     'https://jsd.onmicrosoft.cn/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.css',
   TianliGPT_JS:
     process.env.NEXT_PUBLIC_TIANLI_GPT_JS ||
-    'https://cdn.jsdelivr.net/gh/Dreamofinfinity1/Post-Abstract-AI-NotionNext@master/pastking_1.js',
+    'https://cdn.jsdelivr.net/gh/Dreamofinfinity1/Post-Abstract-AI-NotionNext@master/pastking_2.js',
   TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || 'sssssss',
 
   // 邮件
